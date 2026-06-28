f3c9d60c0299ffdd461582e185a36b9e4b6910ca
James Amyouny <jamesamyouny@gmail.com>
2026-06-28 12:44:56 -0400

    added nodes

---
+++ src/main.py
@@ -0,0 +1,13 @@
+
+
+from textnode import TextNode, TextType
+
+
+def main() -> None:
+    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    print(node)
+
+
+main()
+
+
